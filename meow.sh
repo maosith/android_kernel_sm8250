104bc7f946b0a0bacb060282957020d6f731abe9
maosith <mns.ao@proton.me>
2023-05-05 22:03:18 -0300

    Fixes: Too many declarations were missing for the compiler to continue the build process, here are the fixes.
    ******************************
    modified:   drivers/cpufreq/cpu_input_boost.c
      modified:   drivers/input/input_booster_qc.c
      modified:   drivers/net/ethernet/qualcomm/rmnet/perf/rmnet_perf_opt.c
      modified:   drivers/net/wireless/qualcomm/qca6390/qca-wifi-host-cmn/hif/src/ce/ce_service_srng.c
      modified:   drivers/net/wireless/qualcomm/qca6390/qca-wifi-host-cmn/target_if/core/src/target_if_main.c
      modified:   drivers/net/wireless/qualcomm/qca6390/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
      modified:   drivers/samsung/quest/sec_quest_param.c
      modified:   kernel/module.c
      modified:   security/mstdrv/mstdrv.c
      modified:   security/samsung/defex_lsm/pack_rules
      modified:   security/sdp/dd_core.c
      modified:   techpack/camera/drivers/cam_req_mgr/cam_mem_mgr.c
      modified:   techpack/camera/drivers/cam_req_mgr/cam_req_mgr_util.c
      modified:   techpack/camera/drivers/cam_smmu/cam_smmu_api.c
      modified:   techpack/display/msm/samsung/ss_dsi_panel_debug.c
      modified:   techpack/display/msm/sde/sde_kms.c
    ***********************************

--- meow.sh
+++ meow.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 TC_PATH="$HOME/Clang-16.0.5/bin/"
 
 BUILD_ENV="CC=$(echo $TC_PATH)clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=$(echo $TC_PATH)aarch64-linux-gnu-"
@@ -20,6 +21,17 @@ make -j$(nproc --all) O=out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV Image.gz
 make -j$(nproc --all) O=out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV dtbs
 
 cp arch/arm64/boot/Image.gz AnyKernel3/Image.gz
+=======
+TC_PATH="/home/mao-server/Downloads/"
+
+BUILD_ENV="CC=$(echo $TC_PATH)clang-r475365b/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=$(echo $TC_PATH)gcc-linaro-7.4.1/gcc/bin/aarch64-linux-gnu-"
+
+make O=out ARCH=arm64 $BUILD_ENV r8q_defconfig
+
+make -j$(nproc --all) O=out ARCH=arm64 $BUILD_ENV Image.gz
+
+make -j$(nproc --all) O=out ARCH=arm64 $BUILD_ENV dtbs
+>>>>>>> 8d96fc9dd195 (Fixes: Too many declarations were missing for the compiler to continue the build process, here are the fixes.)
 
 DTB_OUT="out/arch/arm64/boot/dts/vendor/qcom"
 
