36da3804c62b5e9b4f6593ff11ebe468a7c4f4aa
maosith <mns.ao@proton.me>
2023-05-30 20:45:19 -0300

    Revert "treewide: Fix remaining misleading-indentation warnings"
    This reverts commit aae24bc87f63ebac60a237764b28c5cd7de4d2d8.

--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -1930,11 +1930,6 @@ static inline long qcedev_ioctl(struct file *file,
 			err = -EFAULT;
 			goto exit_free_qcedev_areq;
 		}
-
-
-		}
-		}
-
 		break;
 
 	case QCEDEV_IOCTL_SHA_FINAL_REQ:
@@ -2027,11 +2022,6 @@ static inline long qcedev_ioctl(struct file *file,
 			err = -EFAULT;
 			goto exit_free_qcedev_areq;
 		}
-
-
-		}
-		}
-
 		break;
 
 	case QCEDEV_IOCTL_MAP_BUF_REQ:
